c8124ec1279c74b27309e1868865642f08fe46dd
Nillar <martin.p.nikolov@gmail.com>
2017-06-06 14:02:27 +0300

    05Ex_StringsAndRegex 10_SecretData solved

--- 05Lab_StringAndRegEx/test.js
+++ 05Lab_StringAndRegEx/test.js
@@ -1,12 +1,7 @@
-function htmlList(items) {
-    return "<ul>\n" +
-        items.map(htmlEscape).map(
-            item => `  <li>${item}</li>`).join("\n") +
-        "</ul>\n";
-    function htmlEscape(text) {
-        let map = { '"': '&quot;', '&': '&amp;',
-            "'": '&#39;', '<': '&lt;', '>': '&gt;' };
-        return console.log(text.replace(/[\"&'<>]/g, ch => map[ch]));
-    }
+var str = '/abc/hellothere/andmore/';
+var reg = new RegExp('/abc/[^/]*/');
+var matches = str.match(reg);
+
+if (matches && matches.length) {
+    console.log(matches[0].length);
 }
-htmlList(["<br>", "It's OK"]);
