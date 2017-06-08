b8ae3428d734f0110e43115829343f1ccbb2e79b
Nillar <martin.p.nikolov@gmail.com>
2017-06-08 21:33:51 +0300

    06Lab_ObjectsAndJSON problem 7 solved

--- 06Lab_ObjectsAndJSON/07_PopulationInTowns.js
+++ 06Lab_ObjectsAndJSON/07_PopulationInTowns.js
@@ -1,3 +1,19 @@
 function populationCount(input) {
-    
+    let populationMap = new Map();
+    for (let obj of input) {
+        let [town, population] = obj.split(/\s*<->\s*/);
+        population = Number(population);
+        if(populationMap.has(town)){
+            populationMap.set(town, populationMap.get(town) + population);
+        }
+        else {
+            populationMap.set(town, population);
+        }
+    }
+
+    for (let [town, population] of populationMap) {
+        console.log(town + ' : ' + population);
+    }
 }
+
+populationCount(['B<->20', 'A<->30', 'B<->5']);
